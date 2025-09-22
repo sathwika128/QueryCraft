9e19dfef29f381c56695aefcc80d7dfc9c8c935f
Sathwika Kaparthi <112243457+sathwika128@users.noreply.github.com>
2025-09-22 21:42:31 +0530

    Create Exchange_Seats.sql

---
+++ solutions/Exchange_Seats.sql
@@ -0,0 +1,21 @@
+with tmp as(
+    select count(1) cnt from seat
+),tmp1 as(
+select student,case when id%2=0 then id-1
+when (id%2=1 and id=cnt) then id
+when id%2=1 then id+1 end as id
+from seat s 
+cross join tmp t
+)
+select id,student from tmp1 order by 1
+
+
+----------------------
+with tmp1 as(
+select student,case when id%2=0 then id-1
+when (id%2=1 and id=cnt) then id
+when id%2=1 then id+1 end as id
+from seat s
+cross join (select count(1) cnt from seat) t
+)
+select id,student from tmp1 order by 1
