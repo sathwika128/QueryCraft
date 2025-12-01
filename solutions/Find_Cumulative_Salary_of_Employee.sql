77b4c36b8ca864f12cf1dcd2a283d8e38b511ae0
Sathwika Kaparthi <112243457+sathwika128@users.noreply.github.com>
2025-12-01 20:36:48 +0530

    Update Find_Cumulative_Salary_of_Employee.sql

--- solutions/Find_Cumulative_Salary_of_Employee.sql
+++ solutions/Find_Cumulative_Salary_of_Employee.sql
@@ -25,14 +25,11 @@ from employee e
 join tmp t on e.id=t.id
 where concat(t.id,t.latest_month)<>concat(e.id,e.month)
 order by e.id  
-),
-tmp2 as(
-select id,month,salary,month-lag(month) over(partition by id order by month) month_diff,
-sum(salary) over(partition by id order by month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) rolling_sum
-from tmp1
 )
-select id,month,
-case when month_diff is null or month_diff=1 then rolling_sum
-else salary end as salary
-from tmp2
+select a.id,a.month,
+(select sum(b.salary)
+from tmp1 b where a.id=b.id and b.month between a.month-2 and a.month
+) as salary
+from tmp1 a
 order by id asc,month desc
+
