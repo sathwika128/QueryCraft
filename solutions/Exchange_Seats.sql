29885a61cf0869e51875d430323d5cbb06217a64
Sathwika Kaparthi <112243457+sathwika128@users.noreply.github.com>
2025-12-01 20:47:38 +0530

    Update Exchange_Seats.sql

--- solutions/Exchange_Seats.sql
+++ solutions/Exchange_Seats.sql
@@ -19,3 +19,11 @@ from seat s
 cross join (select count(1) cnt from seat) t
 )
 select id,student from tmp1 order by 1
+
+-----------------
+select 
+case when id%2=1 and id=(select count(1) from seat) then id
+when id%2=1 then id+1
+when id%2=0 then id-1 end as id,student
+from seat
+order by id
